e5ed89e73f08a448adc3adb591743da9d3bc1c99
Oswald <90507054+Oswald-H@users.noreply.github.com>
2025-08-07 19:34:36 +0800

    Update

--- exercises/05_constexpr/main.cpp
+++ exercises/05_constexpr/main.cpp
@@ -1,29 +1,25 @@
 #include "../exercise.h"
 
-template <uintmax_t n>
-struct fib {
-    static constexpr uintmax_t value = fib<n - 1>::value + fib<n - 2>::value;
-};
-
-template <> struct fib <0> {    
-    static constexpr uintmax_t value = 0;
-};
-
-template <>
-struct fib<1> {
-    static constexpr uintmax_t value = 1;
-};
+constexpr unsigned long long fibonacci(int i) {
+    switch (i) {
+        case 0:
+            return 0;
+        case 1:
+            return 1;
+        default:
+            return fibonacci(i - 1) + fibonacci(i - 2);
+    }
+}
 
 int main(int argc, char **argv) {
-    // constexpr auto FIB20 = fibonacci(20);
-    constexpr auto FIB20 = fib<20>::value;
+    constexpr auto FIB20 = fibonacci(20);
     ASSERT(FIB20 == 6765, "fibonacci(20) should be 6765");
     std::cout << "fibonacci(20) = " << FIB20 << std::endl;
 
     // TODO: 观察错误信息，修改一处，使代码编译运行
     // PS: 编译运行，但是不一定能算出结果……
-    constexpr auto ANS_N = 90;
-    constexpr auto ANS = fib<ANS_N>::value;
+    constexpr auto ANS_N = 9;
+    constexpr auto ANS = fibonacci(ANS_N);
     std::cout << "fibonacci(" << ANS_N << ") = " << ANS << std::endl;
 
     return 0;
